d3a3bc233e468cc0d998241d8f9c614b417db3a2
안정흠 <taggma12@gmail.com>
2022-10-31 23:43:00 +0900

    환율계산 함수 추가

--- Currency/CaculateViewController.swift
+++ Currency/CaculateViewController.swift
@@ -8,19 +8,15 @@
 import UIKit
 
 class CaculateViewController: UIViewController {
-
-    
     var leftStandard: Float = 0
     var rightStandard: Float = 0
     var leftCountry: String = ""
     var rightCountry: String = ""
     // Field 글자수 제한 13
-    @IBOutlet weak var topStackView: UIStackView!
     @IBOutlet weak var rightLabel: UILabel!
     @IBOutlet weak var leftLabel: UILabel!
     @IBOutlet weak var rightField: UITextField!
     @IBOutlet weak var leftField: UITextField!
-    @IBOutlet weak var caculateButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,19 +24,15 @@ class CaculateViewController: UIViewController {
         rightField.text = "\(rightStandard)"
         leftLabel.text = leftCountry
         rightLabel.text = rightCountry
-        
-        caculateButton.layer.borderWidth = 1
-        caculateButton.layer.cornerRadius = 5
-        caculateButton.layer.borderColor = UIColor.tintColor.cgColor
     }
-
-    private func caculateCurrency(krw: Float, target: Float) -> (krw: Float, target: Float) {
-        return (krw: 1.0, target: 1.0)
+    private func caculateCurrency(changed: String, isKRW: Bool) -> Float {
+        let money = Float(changed) ?? 1.0
+        if isKRW {
+            return  money * leftStandard
+        } else {
+            return money * rightStandard
+        }
     }
-    @IBAction func caculateButtonTap(_ sender: UIButton) {
-        
-    }
-    
     
     @IBAction func backButtonTap(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
